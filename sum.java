4aba4658096034c32e5fe842a2d31d8adc7a7181
unknown <mohamedkamaludeen48@gmail.com>
2023-01-03 14:22:39 +0530

    my

--- sum.java
+++ sum.java
@@ -5,7 +5,7 @@ class sum
         int a=10;
         int b=20;
         int sum=a+b;
-        System.out.println("The Sum of the Result is: "+sum);ytuj
+        System.out.println("The Sum of the Result is: rtgyt"+sum);
         System.out.println("I love Git Hub");//This is pull
         System.out.println("This is test purpes");//This is test
     }
