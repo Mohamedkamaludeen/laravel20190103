42b289565a4a5753ffcc47315348b2ba0aa10e6e
Mohamed Kamaludeen <106368079+Mohamedkamaludeen@users.noreply.github.com>
2023-01-03 14:18:42 +0530

    rfgdfg

--- sum.java
+++ sum.java
@@ -5,7 +5,7 @@ class sum
         int a=10;
         int b=20;
         int sum=a+b;
-        System.out.println("The Sum of the Result is: "+sum);
+        System.out.println("The Sum of the Result is: "+sum);gtgtg
         System.out.println("I love Git Hub");//This is pull
         System.out.println("This is test purpes");//This is test
     }
